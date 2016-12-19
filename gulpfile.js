49aef2d79ee929c8e180f07d2fa70d522e8be393
Celso Mireles <celso.mireles@gmail.com>
2016-12-19 16:30:36 -0700

    Add aos library

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,14 @@ var paths = {
 	harpCompiled: 'www/'
 };
 
+gulp.task('setup', function(){
+	gulp.src('node_modules/aos/dist/aos.css')
+		.pipe(gulp.dest('_harp/public/assets/css/'));
+
+	gulp.src('node_modules/aos/dist/aos.js')
+		.pipe(gulp.dest('_harp/public/assets/js/'));
+});
+
 
 // Live Reload
 
