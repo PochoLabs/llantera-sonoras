db27e25cc35c6cc52c09eb939a83a8c48d948f83
Celso Mireles <celso.mireles@gmail.com>
2016-12-23 01:44:49 -0700

    Add console easter egg

--- _harp/public/assets/js/main.js
+++ _harp/public/assets/js/main.js
@@ -1,5 +1,3 @@
-console.log('You got work to do...');
-
 AOS.init({
   duration: 1200
 });
@@ -1503,3 +1501,32 @@ ctx.restore();
 };
 
 draw(ctx);
+
+
+(function(url) {
+  // Create a new `Image` instance
+  var image = new Image();
+
+  image.onload = function() {
+    // Inside here we already have the dimensions of the loaded image
+    var style = [
+      // Hacky way of forcing image's viewport using `font-size` and `line-height`
+      'font-size: 1px;',
+      'line-height: ' + this.height + 'px;',
+
+      // Hacky way of forcing a middle/center anchor point for the image
+      'padding: ' + this.height * .5 + 'px ' + this.width * .5 + 'px;',
+
+      // Set image dimensions
+      'background-size: ' + this.width + 'px ' + this.height + 'px;',
+
+      // Set image URL
+      'background: url('+ url +');'
+     ].join(' ');
+
+     console.log('%c', style);
+  };
+
+  // Actually loads the image
+  image.src = url;
+})('http://pocholabs.com/img/logo.svg');
